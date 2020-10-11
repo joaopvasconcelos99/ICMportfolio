a4ed928fe622ab1054f8036dabd9c4537e1c93e6
João Pedro Vasconcelos <47739139+joaopvasconcelos99@users.noreply.github.com>
2020-10-11 04:17:12 +0100

    Lab01-1-FirstApp

---
+++ lab01_1-FirstApp/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void  onResume() {
+
+        super.onResume();
+
+        Log.i("MainActivity", " \t got onResume callback");
+    }
+
+    @Override
+    protected void  onPause() {
+
+        super.onPause();
+
+        Log.i("MainActivity", " \t got onPause callback");
+    }
+
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
